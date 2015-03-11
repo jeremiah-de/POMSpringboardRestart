429be51722f51dbb740c2671b48cafa1b98d33b3
Jeremiah <jeremiah@doubleencore.com>
2015-03-11 16:00:19 -0600

    springboard restart

--- POMSpringboardRestart/ViewController.m
+++ POMSpringboardRestart/ViewController.m
@@ -10,18 +10,59 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) IBOutlet UICollectionView *collectionView;
+
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+#pragma mark - Setup & Teardown
+
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    CGRect collectionViewFrame = CGRectMake(0, 0, self.view.frame.size.width, [self itemSize].height * [self collectionView:self.collectionView numberOfItemsInSection:0]);
+    self.collectionView = [[UICollectionView alloc] initWithFrame:collectionViewFrame collectionViewLayout:[UICollectionViewFlowLayout new]];
+    self.collectionView.dataSource = self;
+    self.collectionView.delegate = self;
+    self.collectionView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [self.view addSubview:self.collectionView];
+    
+    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"cell"];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - Private
+
+- (CGSize)itemSize
+{
+    return CGSizeMake(self.collectionView.frame.size.width, 50.0f);
+}
+
+#pragma mark - UICollectionViewDataSource
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 1000;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"cell" forIndexPath:indexPath];
+    cell.backgroundColor = [UIColor whiteColor];
+    return cell;
+}
+
+#pragma mark - UICollectionViewDelegateFlowLayout
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return [self itemSize];
 }
 
 @end
